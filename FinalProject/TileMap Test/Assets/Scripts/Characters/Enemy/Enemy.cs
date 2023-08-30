2d84d6d29f6f918654fd169b0e9d8ed68ac52312
MiguelAyala25 <tlacuacheveloz25@gmail.com>
2023-08-30 14:25:12 -0700

    combat

--- FinalProject/TileMap Test/Assets/Scripts/Characters/Enemy/Enemy.cs
+++ FinalProject/TileMap Test/Assets/Scripts/Characters/Enemy/Enemy.cs
@@ -6,5 +6,17 @@ public class Enemy : MonoBehaviour
 {
     public int currentHP = 100;
 
-    
+    // Reduce hp 
+    public void TakeDamage(int damage)
+    {
+        currentHP -= damage;
+    }
+
+    // Destroy enemy
+    public void DestroyEnemy()
+    {
+        Destroy(gameObject); 
+    }
+
+
 }
